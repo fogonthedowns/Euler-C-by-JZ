cbc2b185f3f3006913c9a5d9b05e83252bc54597
fogonthedowns <fogonthedowns@gmail.com>
2012-08-05 19:31:13 -0700

    Initial Commit

---
+++ project euler problem 1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  project euler problem 1
+//
+//  Created by Justin Zollars on 8/5/12.
+//  Copyright (c) 2012 Justin Zollars. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
